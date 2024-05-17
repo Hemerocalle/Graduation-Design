c90b21204e7f2aa667fb720654cafd29232e8c4a
Hemerocalle <2958238716@qq.com>
2024-05-17 17:09:22 +0800

    修改部分注释

--- controller.py
+++ controller.py
@@ -9,7 +9,20 @@
 # Controller
 # 识别器的控制器，用于对外提供接口并协调三个识别器的工作状态
 #
-# 最后更新时间 2024/04/15
+# radio_set
+# 根据给定的索引设置语音识别器类型，并相应地更新用户界面状态
+#
+# prepare
+# 初始化和重置界面元素状态以准备开始任务
+#
+# loadImg
+# 根据当前模型状态加载图像，处理图像加载过程中的各种结果
+# 并更新UI状态来反映加载结果
+#
+# detectImg
+# 识别图像中的人脸并进行相应处理
+#
+# 最后更新时间 2024/05/17
 
 from PyQt5.QtWidgets import QLabel, QPushButton
 
@@ -40,25 +53,29 @@ class Controller():
         cls.rec.radioOn()
         cls.enable = {'model': False, 'image': False}
 
-    # 切换模式
+    # 根据给定的索引设置语音识别器类型，并相应地更新用户界面状态
     @classmethod
     def radio_set(cls, index: str) -> None:
         if type(cls.rec) is cls.mode[RECOGNIZER_TYPE[index]]:
             return
+
+        # 根据索引设置新的识别器类型
         cls.rec = cls.mode[RECOGNIZER_TYPE[index]](cls.rec)
         cls.enable['image'] = False
         if cls.enable['model'] is False:
             return
 
+        # 更新用户界面显示
         cls.textBox.setText('数据框')
         cls.imageBox.setStyleSheet('border: 3px solid black;')
         cls.imageBox.setText('图像框')
         cls.startButton.setEnabled(all(cls.enable.values()))
 
-    # 模型载入
+    # 初始化和重置界面元素状态以准备开始任务
     @classmethod
     def prepare(cls, prepareButton: QPushButton, startButton: QPushButton,
                 imageBox: QLabel, textBox: QLabel) -> None:
+        # 启用模型并设置界面元素
         cls.enable['model'] = True
         cls.imageBox = imageBox
         cls.textBox = textBox
@@ -67,13 +84,16 @@ class Controller():
         prepareButton.setEnabled(False)
         cls.textBox.setText(Result.PREPARE.value)
 
-    # 资源导入
+    # 根据当前模型状态加载图像，处理图像加载过程中的各种结果
+    # 并更新UI状态来反映加载结果
     @classmethod
     def loadImg(cls) -> None:
-        if cls.enable['model'] is False:  # 如果没有载入模型则退出
+        if cls.enable['model'] is False:  # 检查模型是否已载入，未载入则直接返回
             return
         res, text = cls.rec.loadImg()  # type: ignore
-        if res is Result.FINISH:  # 导入完成
+
+        # 根据加载图像的结果更新UI状态
+        if res is Result.FINISH:  # 图像加载完成
             cls.enable['image'] = True
             cls.textBox.setText(text)
             cls.imageBox.setStyleSheet(f'image: url(./{PATH_ORIGIN})')
@@ -90,6 +110,7 @@ class Controller():
             cls.imageBox.setText('图像框')
         elif res is Result.LOADING:  # 请等待
             cls.textBox.setText(Result.LOADING.value)
+            # 重新尝试加载图像
             res, text = cls.rec.loadImg(Result.LOADING)  # type: ignore
             if res is Result.CAMERA_START:
                 cls.enable['image'] = True
@@ -109,12 +130,13 @@ class Controller():
             cls.imageBox.setText('图像框')
         cls.startButton.setEnabled(all(cls.enable.values()))
 
-    # 开始识别
+    # 识别图像中的人脸并进行相应处理
     @classmethod
     def detectImg(cls) -> None:
         while cls.enable['image']:
+            # 调用识别函数检测图像，获取结果和识别到的文本
             res, text = cls.rec.detectImg()  # type: ignore
-            if cls.enable['image'] is False:  # 如果瞬间切换页面则退出
+            if cls.enable['image'] is False:  # 如果检测到瞬间的页面切换，则退出
                 return
             if res is Result.FINISH:  # 识别完成，停止工作
                 cls.textBox.setText(text)
